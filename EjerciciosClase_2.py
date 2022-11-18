b0642480c2d254dbb12245c0ec0e9a8cedab62f0
h4che404 <eliasjuancruz54@gmail.com>
2022-11-18 18:48:39 -0300

    EjerciciosClase_2.py

--- EjerciciosClase_2.py
+++ EjerciciosClase_2.py
@@ -2,23 +2,37 @@
 var = 1
 print(var)
 #2
+print("--------------------------------")
 var1 = 8.5
 print(type(var1))
 #3
+print("--------------------------------")
 print(var)
 #4
+print("--------------------------------")
 nombre = "juan"
 #5
+print("--------------------------------")
 num = 9123.123423
 #6
+print("--------------------------------")
 print(type(num))
 #7
+print("--------------------------------")
 palabra = "juan"
 mensaje = palabra.find("a")
 print (mensaje)
 #8
+print("--------------------------------")
 x = 0
 while x < 3:
     x += 1
     print(x)
+print("fin")
+#9
+print("--------------------------------")
+for i  in cadena("a"):
+    nombre1 = input("ingrese un nombre")
+    edad= input ("ingrese su edad")
+    print("Hola {}, usted tiene {}?".format(nombre1, edad))
 print("fin")
