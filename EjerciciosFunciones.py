9228d8f7e237620feaba785a6c969927047b61a0
Juan Cruz Elias <eliasjuancruz54@gmail.com>
2022-12-15 21:02:58 -0300

    Agregamos funciones hechas por las IA

--- EjerciciosFunciones.py
+++ EjerciciosFunciones.py
@@ -88,7 +88,7 @@ nombres=['juan', 'karla', 'guillermo']
 '''
 
 #Factorial de 5
-
+'''
 def factorial (numero):
     if numero == 1:
         return 1
@@ -97,3 +97,32 @@ def factorial (numero):
     
 resultado = factorial(5)
 print(f'El factorial es: {resultado}')
+'''
+
+# Define a function to calculate the factorial of a number
+'''
+def factorial(n):
+  # Set the initial value of the factorial to 1
+  fact = 1
+  
+  # Use a for loop to multiply the factorial by each number from 1 to n
+  for i in range(1, n+1):
+    fact *= i
+  
+  # Return the factorial
+  return fact
+
+# Calculate the factorial of 5
+factorial_5 = factorial(5)
+
+# Print the result
+print(factorial_5)
+'''
+
+numbers = [1, 2, 3, 4, 5]
+
+# elevar al cuadrado cada elemento de la lista `numbers`
+squares = list(map(pow, numbers, [2] * len(numbers)))
+
+# imprimir el resultado
+print(squares)  # [1, 4, 9, 16, 25]
